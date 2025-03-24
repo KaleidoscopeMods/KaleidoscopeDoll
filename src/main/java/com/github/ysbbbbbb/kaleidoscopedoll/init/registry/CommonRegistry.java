638dbb6291c74157e87b7f1dddad6769ec60e36b
Azumic <775300932@qq.com>
2025-03-24 13:07:05 +0800

    初步完成curios的兼容 - 补齐赞助者玩偶的掉落物

---
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/init/registry/CommonRegistry.java
@@ -0,0 +1,17 @@
+package com.github.ysbbbbbb.kaleidoscopedoll.init.registry;
+
+import com.github.ysbbbbbb.kaleidoscopedoll.KaleidoscopeDoll;
+import com.github.ysbbbbbb.kaleidoscopedoll.compat.curios.CuriosCompat;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = KaleidoscopeDoll.MOD_ID)
+public class CommonRegistry {
+
+    @SubscribeEvent
+    public static void onSetupEvent(FMLCommonSetupEvent event) {
+        event.enqueueWork(CuriosCompat::commonSetup);
+    }
+
+}

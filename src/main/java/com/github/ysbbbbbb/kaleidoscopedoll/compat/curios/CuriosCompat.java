84084244b59f7352085700309c0d308e6427edfd
Azumic <95330698+Azumic@users.noreply.github.com>
2025-03-24 19:06:14 +0800

    Merge pull request #1
    * 初步完成curios的兼容
    
    * 格式小修正

---
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/compat/curios/CuriosCompat.java
@@ -0,0 +1,28 @@
+package com.github.ysbbbbbb.kaleidoscopedoll.compat.curios;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.fml.ModList;
+
+public class CuriosCompat {
+    private static final String ID = "curios";
+    private static final boolean IS_LOADED;
+
+    static {
+        IS_LOADED = ModList.get().isLoaded(ID);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static void addEntityLayers(EntityRenderersEvent.AddLayers event) {
+        if (IS_LOADED) {
+            CuriosCompatInner.registerRenderer(event);
+        }
+    }
+
+    public static void commonSetup() {
+        if (IS_LOADED) {
+            CuriosCompatInner.registerDollItemPredicate();
+        }
+    }
+}
---
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/compat/curios/CuriosCompatInner.java
@@ -0,0 +1,27 @@
+package com.github.ysbbbbbb.kaleidoscopedoll.compat.curios;
+
+import com.github.ysbbbbbb.kaleidoscopedoll.KaleidoscopeDoll;
+import com.github.ysbbbbbb.kaleidoscopedoll.item.DollItem;
+import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import top.theillusivec4.curios.api.CuriosApi;
+
+public class CuriosCompatInner {
+    @OnlyIn(Dist.CLIENT)
+    static void registerRenderer(EntityRenderersEvent.AddLayers event) {
+        if (event.getSkin("default") instanceof PlayerRenderer playerRenderer) {
+            playerRenderer.addLayer(new DollItemRenderer<>(playerRenderer, event.getContext().getItemInHandRenderer()));
+        }
+        if (event.getSkin("slim") instanceof PlayerRenderer playerRenderer) {
+            playerRenderer.addLayer(new DollItemRenderer<>(playerRenderer, event.getContext().getItemInHandRenderer()));
+        }
+    }
+
+    static void registerDollItemPredicate() {
+        CuriosApi.registerCurioPredicate(new ResourceLocation(KaleidoscopeDoll.MOD_ID, "doll_item"),
+                slotResult -> slotResult.stack().getItem() instanceof DollItem);
+    }
+}

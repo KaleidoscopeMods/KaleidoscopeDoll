1956f367e84a87cde32f13d213b4b50e94b8c2ac
tartaric_acid <baka943@qq.com>
2025-04-18 18:04:41 +0800

    更新一批玩家模型

--- src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/datagen/DataGenerators.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/datagen/DataGenerators.java
@@ -34,5 +34,6 @@ public class DataGenerators {
                 pack, registries, existingFileHelper,
                 Collections.singletonList(new AdvancementGenerator())
         ));
+        generator.addProvider(event.includeClient(), new ItemModelGenerator(pack, existingFileHelper));
     }
 }

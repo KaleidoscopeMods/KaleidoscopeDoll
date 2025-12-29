28ef874c0839661712bca456578a305cd4b28f08
tartaric_acid <baka943@qq.com>
2025-12-29 20:17:54 +0800

    重载指令运行时能够重载创造模式标签页

--- src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/network/message/CustomDollReloadMessage.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/network/message/CustomDollReloadMessage.java
@@ -2,12 +2,21 @@ package com.github.ysbbbbbb.kaleidoscopedoll.network.message;
 
 import com.github.ysbbbbbb.kaleidoscopedoll.KaleidoscopeDoll;
 import com.github.ysbbbbbb.kaleidoscopedoll.client.custom.CustomDollLoader;
+import com.github.ysbbbbbb.kaleidoscopedoll.init.ModCreativeTabs;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.network.NetworkEvent;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 public class CustomDollReloadMessage {
@@ -30,6 +39,27 @@ public class CustomDollReloadMessage {
     private static void onReload() {
         try {
             CustomDollLoader.init();
+
+            Minecraft minecraft = Minecraft.getInstance();
+            FeatureFlagSet features = Optional.ofNullable(minecraft.player)
+                    .map(p -> p.connection)
+                    .map(ClientPacketListener::enabledFeatures)
+                    .orElse(FeatureFlagSet.of());
+
+            final boolean hasOperatorItemsTabPermissions =
+                    minecraft.options.operatorItemsTab().get() ||
+                    Optional.of(minecraft)
+                            .map(m -> m.player)
+                            .map(Player::canUseGameMasterBlocks)
+                            .orElse(false);
+
+            ClientLevel level = minecraft.level;
+            if (level != null) {
+                RegistryAccess registryAccess = level.registryAccess();
+                final CreativeModeTab.ItemDisplayParameters parameters =
+                        new CreativeModeTab.ItemDisplayParameters(features, hasOperatorItemsTabPermissions, registryAccess);
+                ModCreativeTabs.CUSTOM_DOLL_TAB.get().buildContents(parameters);
+            }
         } catch (IOException e) {
             KaleidoscopeDoll.LOGGER.error("Failed to reload custom dolls", e);
         }

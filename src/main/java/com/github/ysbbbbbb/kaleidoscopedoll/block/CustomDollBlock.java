ed0cd940ef63f61ab7cc036781658693b9139b85
tartaric_acid <baka943@qq.com>
2026-03-10 20:50:34 +0800

    修正自定义玩偶不掉落的问题

--- src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/block/CustomDollBlock.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/block/CustomDollBlock.java
@@ -2,6 +2,7 @@ package com.github.ysbbbbbb.kaleidoscopedoll.block;
 
 import com.github.ysbbbbbb.kaleidoscopedoll.block.entity.CustomDollBlockEntity;
 import com.github.ysbbbbbb.kaleidoscopedoll.item.CustomDollItem;
+import com.google.common.collect.Lists;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -9,9 +10,14 @@ import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.phys.HitResult;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
+
+@SuppressWarnings("deprecation")
 public class CustomDollBlock extends DollBlock implements EntityBlock {
     @Override
     public @Nullable BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
@@ -25,5 +31,18 @@ public class CustomDollBlock extends DollBlock implements EntityBlock {
             CustomDollItem.setModelId(stack, blockEntity.getModelId());
         }
         return stack;
+    }
+
+    @Override
+    public List<ItemStack> getDrops(BlockState state, LootParams.Builder params) {
+        // 因为可能是不可变 List，故复制一份
+        List<ItemStack> stacks = Lists.newArrayList(super.getDrops(state, params));
+        BlockEntity blockEntity = params.getOptionalParameter(LootContextParams.BLOCK_ENTITY);
+        if (blockEntity instanceof CustomDollBlockEntity be) {
+            ItemStack stack = new ItemStack(this);
+            CustomDollItem.setModelId(stack, be.getModelId());
+            stacks.add(stack);
+        }
+        return stacks;
     }
 }
